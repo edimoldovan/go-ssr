1b540e5847de9bdb5228df4d7ce72d7c03e1c920
ed <edimoldovan@gmail.com>
2023-01-28 20:59:00 +0100

    simplify and embed templates and public files

--- server.go
+++ server.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"embed"
 	"html/template"
+	"io/fs"
 	"log"
 	"main/handlers"
 	"main/middlewares"
-	"main/utilities"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
+//go:embed templates
+var embededTemplates embed.FS
+
+//go:embed public
+var embededPublic embed.FS
+
 func main() {
-	// read template files
-	templateFiles := utilities.GetTemplates()
-	// parse template files
-	handlers.Tmpl, _ = template.ParseFiles(templateFiles...)
+	// pre-parse templates, embedded in server binary
+	handlers.Tmpl = template.Must(template.ParseFS(embededTemplates, "templates/layouts/*.html", "templates/partials/*.html"))
+
 	// router
 	router := httprouter.New()
 
@@ -26,9 +32,12 @@ func main() {
 	// HTML routes
 	router.GET("/", middlewares.Wrapper(chain.ThenFunc(handlers.Home)))
 
-	// static routes
-	f := utilities.GetExecutablePath()
-	router.ServeFiles("/public/*filepath", http.Dir(f+"public"))
+	// static routes, embedded in server binary
+	if public, err := fs.Sub(embededPublic, "public"); err == nil {
+		router.Handler("GET", "/public/*filepath", http.StripPrefix("/public", http.FileServer(http.FS(public))))
+	} else {
+		panic(err)
+	}
 
 	// HTTP server
 	log.Fatal(http.ListenAndServe(":8000", router))
